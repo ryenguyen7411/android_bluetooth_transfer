bc9f84d26a77b002d46222b806bccb916a05439d
ryenguyen7411 <ryenguyen7411@gmail.com>
2016-11-05 18:51:29 +0700

    - Commit for sure

--- BluetoothTransfer/app/src/main/java/com/android/rye/bluetoothtransfer/MainActivity.java
+++ BluetoothTransfer/app/src/main/java/com/android/rye/bluetoothtransfer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.android.rye.bluetoothtransfer;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +37,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button btn = (Button) findViewById(R.id.button_temp);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String path = Environment.getExternalStorageDirectory().toString()+"/Pictures";
+                Log.d("Files", "Path: " + path);
+                File f = new File(path);
+                File file[] = f.listFiles();
+                Log.d("Files", "Size: "+ file.length);
+                for (int i=0; i < file.length; i++)
+                {
+                    Log.d("Files", "FileName:" + file[i].getName());
+                }
+            }
+        });
+
+        Button btn2 = (Button) findViewById(R.id.button);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String path = Environment.getExternalStorageDirectory().toString()+"/Download";
+                try {
+                    splitFile(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -48,5 +90,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static void splitFile(String filePath) throws IOException {
+        File f = new File(filePath + "/xda-lab.apk");
+        if(!f.isFile()){
+            return;
+        }
+
+        int partCounter = 1;
+
+        int sizeOfFiles = 1024 * 1024;// 1MB
+        byte[] buffer = new byte[sizeOfFiles];
+
+        try (BufferedInputStream bis = new BufferedInputStream(
+                new FileInputStream(f))) {
+            String name = f.getName();
+
+            int tmp = 0;
+            while ((tmp = bis.read(buffer)) > 0) {
+                //write each chunk of data into separate file with different number in name
+                File newFile = new File(f.getParent(), name + "."
+                        + String.format("%03d", partCounter++));
+                try (FileOutputStream out = new FileOutputStream(newFile)) {
+                    out.write(buffer, 0, tmp);//tmp is chunk size
+                }
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static void mergeFiles(String sharePath) throws IOException {
+
     }
 }
